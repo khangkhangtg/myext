8e9dd453f5585b00c68b95bfab84a2d36069ce16
Kit Hoang <khangkhangtg@gmail.com>
2020-06-03 17:11:38 +0700

    using array Timer instead of adding Timer into Tab Object

--- background_script.js
+++ background_script.js
@@ -1,53 +1,12 @@
 // Put all the javascript code here, that you want to execute in background.
 
-function notifyMessage (noti) {
-	console.log("background script received message");
-	browser.notifications.create({
-		"type": "basic",
-		"title": "Timer notification",
-		"message": `You are spending ${noti} seconds for this website.`
-	});
-}
-
-function isValidHttpUrl(string) {
-	let url;
-
-	try {
-		url = new URL(string);
-	} catch (_) {
-		return false;  
-	}
-
-	return url.protocol === "http:" || url.protocol === "https:";
-}
-
-function onGot(tabInfo) {
-  console.log(tabInfo);
-}
-
-function onError(error) {
-	console.log(`Error: ${error}`);
-}
-
-function getAllInfo () {
-	let tabs = browser.tabs.query({});
-	tabs.then((tabs) => {
-		for(let tab of tabs) {
-			console.log(tab);
-		}
-	});
-}
+let tabsTimer = [], testT = [];
 
 function addTimer () {
-	let timer = 0;
-	let count;
+	let timer = 0, count;
 
-	let countSecond = function () {
+	const countSecond = function () {
 		timer++;
-	}
-
-	let test = function () {
-		notifyMessage(timer);
 	}
 
 	return {
@@ -58,60 +17,59 @@ function addTimer () {
 			count = setInterval(countSecond, 1000);
 		},
 		pause: function () {
-			if (count) clearInterval(count);
+			if(count) clearInterval(count);
 		},
 		reset: function () {
+			this.pause();
 			timer = 0;
-		},
-		test: function () {
-			test();
+			this.start();
 		}
 	}
 }
 
-function initTabs (tabs) {
-	for (let tab of tabs) {
-		if (!isValidHttpUrl(tab.url)) continue;
-
-		tab.timer = addTimer();
-		tab.timer.start();
-		
-		console.log(tabs);
-
-		setInterval(() => {
-			console.log(tab.timer.getTimer());
-		}, 15000);
-	}
+function removeTimer (tabId) {
+	const tmpTimer = timer.splice(tabsTimer.indexOf(tabId), 1);
+	tabsTimer = tmpTimer;
 }
 
-// add timer for all tabs when opening the browser
-function init () {
-	let tabs = browser.tabs.query({});
-	tabs.then((tabs) => {
-		initTabs(tabs);
+function testTimer () {
+	tabsTimer.forEach( function(timer, index) {
+		// console.log(timer);
+		// console.log(index);
+		testT[index] = setInterval(() => {console.log(timer.getTimer());}, 10000);
 	});
 }
 
-init();
+function resetTestTimer (id) {
+	clearInterval(testT[id]);
+	testT[id] = setInterval(() => {console.log(tabsTimer[id].getTimer());}, 10000);
+}
 
-// reset timer when tab's url updates
-browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-	if(changeInfo.url) {
-		// console.log(tab);
-		// let tabInfo = browser.tabs.get(tabId);
-		// tabInfo.then((info) => {
-		// 	console.log(info);
-		// });
-
-		let tabs = browser.tabs.query({});
-		tabs.then((tabs) => {
-			for(let tab of tabs) {
-				console.log(tab);
-			}
+function initTimer() {
+	let gettingTabs = browser.tabs.query({});
+	gettingTabs.then((tabs) => {
+		tabs.map(tab => tab.id).forEach((id) => {
+			tabsTimer[id] = addTimer();
+			tabsTimer[id].start();
 		});
-	}
+		testTimer();
+		// console.log(tabsTimer);
+	});
 
-	if(tab.status === "complete") {
-		console.log(tab);
+}
+
+initTimer();
+
+// add event 'onUpdated' for checking url
+browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+	if (changeInfo.url) {
+		tabsTimer[tabId].reset();
+		// resetTestTimer(tabId);
 	}
 });
+
+// add event 'onCreated' for creating new tab
+browser.tabs.onCreated.addListener()
+
+// add event 'onRemoved' for closing the tab
+browser.tabs.onRemoved.addListener()
